253e86e750c05f967ab5dc80a6acc74549e2f1b0
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-06-17 21:50:07 -0400

    Initial structure and components created.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { FooterComponent } from "./footer/footer.component";
+import { HeroComponent } from "./hero/hero.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [NavbarComponent, HeroComponent, FooterComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'aaronsisler';
+  title = "aaronsisler";
 }
