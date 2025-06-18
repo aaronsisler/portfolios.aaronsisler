df67cc73f5d65f6cdff06922f1ffde2b794d345b
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-06-18 17:37:36 -0400

    Hold this. Need to see how to fix animations

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  selector: "app-navbar",
+  imports: [CommonModule],
+  templateUrl: "./navbar.component.html",
+  styleUrl: "./navbar.component.css",
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  isScrolled: boolean = false;
 
+  private scrollHandler = () => {
+    this.isScrolled = window.scrollY > 50;
+  };
+
+  ngOnInit(): void {
+    window.addEventListener("scroll", this.scrollHandler);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener("scroll", this.scrollHandler);
+  }
+
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
 }
