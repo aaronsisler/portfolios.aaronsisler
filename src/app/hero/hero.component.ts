6091fbe9723c4d0826e65bdae101a6846d68dbaf
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-06-19 16:43:02 -0400

    Adding in the ag-icons and CTA. Need to make icons work links wise

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  bootstrapEnvelopeFill,
+  bootstrapGithub,
+  bootstrapLinkedin,
+} from "@ng-icons/bootstrap-icons";
+import { NgIcon, provideIcons } from "@ng-icons/core";
 
 @Component({
-  selector: 'app-hero',
-  imports: [],
-  templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  selector: "app-hero",
+  imports: [NgIcon],
+  viewProviders: [
+    provideIcons({ bootstrapLinkedin, bootstrapGithub, bootstrapEnvelopeFill }),
+  ],
+  templateUrl: "./hero.component.html",
+  styleUrl: "./hero.component.css",
 })
 export class HeroComponent {
-
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
 }
