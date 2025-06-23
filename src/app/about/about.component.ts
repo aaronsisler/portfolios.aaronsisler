cb850968afe2c74d8da87c478d09184fb1735ed8
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-06-23 15:48:47 -0400

    Adding in the About section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  bootstrapGlobeAmericas,
+  bootstrapLayers,
+  bootstrapLightningCharge,
+} from "@ng-icons/bootstrap-icons";
+import { NgIcon, provideIcons } from "@ng-icons/core";
 
 @Component({
-  selector: 'app-about',
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  selector: "app-about",
+  imports: [NgIcon],
+  viewProviders: [
+    provideIcons({
+      bootstrapLayers,
+      bootstrapGlobeAmericas,
+      bootstrapLightningCharge,
+    }),
+  ],
+  templateUrl: "./about.component.html",
+  styleUrl: "./about.component.css",
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
